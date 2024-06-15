e038c8aaaa3b127c2d69da5213dc3d701f5578a5
Ajay-2000-20 <ajay.jaatchauhan@gmail.com>
2024-06-15 13:19:44 +0530

    Create 1st class.py

---
+++ 1st class.py
@@ -0,0 +1,61 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+5+6
+11
+34-5
+29
+12/3
+4.0
+4*5
+20
+5+6
+11
+
+
+
+5//2
+2
+5%2
+1
+Type "help", "copyright", "credits" or "license()" for more information.
+SyntaxError: invalid syntax
+>>> 
+>>> 5**4
+625
+>>> 8**2
+64
+>>> 5/2+3
+5.5
+>>> 64/4*4
+64.0
+>>> x=1+2*3-4/5**6
+>>> 1+2*3-4/5**6
+6.999744
+>>> a=2
+>>> b=6
+>>> a<b
+True
+>>> b>a
+True
+>>> a==b
+False
+>>> b<a
+False
+>>> a=b
+>>> a
+6
+>>> b
+6
+>>> a=80
+>>> a+=5
+>>> a
+85
+>>> a+=5
+>>> a
+90
+>>> a-=5
+>>> a
+85
+>>> a*=5
+>>> a
+425
